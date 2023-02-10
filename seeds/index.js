add74adc95ee74fbe72914bfc90d3822eb0d47d4
Julissa Bonilla <julissab9@gmail.com>
2023-02-10 14:27:14 -0600

    updated seed folder

---
+++ seeds/index.js
@@ -0,0 +1,32 @@
+// Aquire necessary things to run
+const sequelize = require('../config/connection');
+const { User, Comment, Post } = require('../models');
+
+// Aquire json data from seeds 
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true})
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const posts = await Post.bulkCreate(postData, {
+        individualHooks: true,
+        returning: true,
+    })
+
+    const comments = await Comment.bulkCreate(commentData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+console.log(users, posts, comments);
+    process.exit(0)
+};
+
+seedDatabase();
