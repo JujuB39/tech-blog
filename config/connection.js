eb3dbec3f86e95ff71ae85d7bfd7eec361eccc0b
Julissa Bonilla <julissab9@gmail.com>
2023-02-10 18:59:40 -0600

    updated dashboard handlebars

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ console.log(process.env)
 let sequelize
 console.log(process.env.DB_PASSWORD)
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD)
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const session = require ('express-session');
 const { create } = require('express-handlebars')
-const mysql = require('mysql');
-const mysql2 = require('mysql2')
+
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
