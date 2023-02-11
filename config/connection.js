d6ed6cb927380720f5935ceda5658fbf6b4eccd1
Julissa Bonilla <julissab9@gmail.com>
2023-02-10 18:52:22 -0600

    pushing up for TA help

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
+console.log(process.env)
 let sequelize
-
+console.log(process.env.DB_PASSWORD)
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL)
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD)
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
-            dialect: 'mysql',
             host: 'localhost',
+            dialect: 'mysql',
             port: 3306
         }
     )
